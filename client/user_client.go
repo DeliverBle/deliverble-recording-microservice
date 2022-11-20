cf837d23c6f89c31a05225019005dd754e45d468
Sigrid <sigrid.jinhyung@gmail.com>
2022-11-20 19:34:40 +0900

    feat: add user client for post server to communicate with user grpc

--- client/user_client.go
+++ client/user_client.go
@@ -1,1 +1,25 @@
 package client
+
+import (
+	userpb "deliverble-recording-msa/protos/v1/user"
+	"google.golang.org/grpc"
+	"sync"
+)
+
+var (
+	once sync.Once
+	cli  userpb.UserClient
+)
+
+func GetUserClient(serviceHost string) userpb.UserClient {
+	once.Do(func() {
+		conn, _ := grpc.Dial(serviceHost,
+			grpc.WithInsecure(),
+			grpc.WithBlock(),
+		)
+
+		cli = userpb.NewUserClient(conn)
+	})
+
+	return cli
+}
