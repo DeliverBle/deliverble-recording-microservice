9af0cacf81c6e4cc64a56e7e11af6ba2efc74e6c
Sigrid <sigrid.jinhyung@gmail.com>
2022-11-20 22:38:59 +0900

    feat: add s3Info struct that saves AWS related configuration

--- s3_server/S3Info.go
+++ s3_server/S3Info.go
@@ -1,1 +1,12 @@
 package s3_server
+
+import "github.com/aws/aws-sdk-go-v2/service/s3"
+
+type S3Info struct {
+	AwsS3Region    string
+	AwsAccessKey   string
+	AwsSecretKey   string
+	AwsProfileName string
+	BucketName     string
+	S3Client       *s3.Client
+}
