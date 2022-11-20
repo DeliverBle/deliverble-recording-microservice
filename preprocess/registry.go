1920c0ae259726a077d1c45f2b957751914a0734
Sigrid <sigrid.jinhyung@gmail.com>
2022-11-20 19:35:46 +0900

    feat: add ListPostsByUserId which returns post msgs by user id

--- preprocess/registry.go
+++ preprocess/registry.go
@@ -3,11 +3,17 @@ package preprocess
 import (
 	"context"
 	"deliverble-recording-msa/data"
+	postpb "deliverble-recording-msa/protos/v1/post"
 	userpb "deliverble-recording-msa/protos/v1/user"
 )
 
 type UserServer struct {
 	userpb.UserServer
+}
+
+type PostServer struct {
+	postpb.PostServer
+	UserClient userpb.UserClient
 }
 
 /*
@@ -30,7 +36,7 @@ func (s *UserServer) GetUser(ctx context.Context, req *userpb.GetUserRequest) (*
 	}, nil
 }
 
-func (s *UserServer) ListUsers(ctx context.Context, req *userpb.ListUsersRequest) (*userpb.ListUsersResponse, error) {
+func (s *UserServer) ListUsers(context.Context, *userpb.ListUsersRequest) (*userpb.ListUsersResponse, error) {
 	userMessages := make([]*userpb.UserMessage, len(data.MockUserData))
 	for i, u := range data.MockUserData {
 		userMessages[i] = u
@@ -38,5 +44,40 @@ func (s *UserServer) ListUsers(ctx context.Context, req *userpb.ListUsersRequest
 
 	return &userpb.ListUsersResponse{
 		UserMessages: userMessages,
+	}, nil
+}
+
+/*
+ListPostsByUserId returns post messages by user_id
+*/
+func (s *PostServer) ListPostsByUserId(ctx context.Context, req *postpb.ListPostsByUserIdRequest) (*postpb.ListPostsByUserIdResponse, error) {
+	userID := req.UserId
+
+	response, err := s.UserClient.GetUser(ctx, &userpb.GetUserRequest{
+		UserId: userID,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	var postMessagesByUserId []*postpb.PostMessage
+	for _, up := range data.MockUserPosts {
+		// if not wanted userid, then pass
+		if up.UserId != userID {
+			continue
+		}
+
+		// inject author name
+		for _, post := range up.Posts {
+			post.Author = response.UserMessage.Name
+		}
+
+		// add all posts then break
+		postMessagesByUserId = append(postMessagesByUserId, up.Posts...)
+		break
+	}
+
+	return &postpb.ListPostsByUserIdResponse{
+		PostMessages: postMessagesByUserId,
 	}, nil
 }
