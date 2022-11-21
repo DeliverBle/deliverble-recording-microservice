3756cc4ddb529f7d49d551ba4ad9a457807b9417
Sigrid <sigrid.jinhyung@gmail.com>
2022-11-21 13:32:51 +0900

    test: integration test for upload recording handler

--- test/UploadRecordingHandler_test.go
+++ test/UploadRecordingHandler_test.go
@@ -1,1 +1,51 @@
 package test
+
+import (
+	"bytes"
+	"deliverble-recording-msa/preprocess"
+	"github.com/labstack/echo"
+	"github.com/stretchr/testify/assert"
+	"io"
+	"mime/multipart"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"path/filepath"
+	"testing"
+)
+
+func TestUploadRecordingHandler(t *testing.T) {
+	// given : preparing form file (mp3)
+	assertions := assert.New(t)
+
+	path := "./test.mp3"
+	file, err := os.Open(path)
+	assertions.NoError(err)
+	defer file.Close()
+
+	buf := &bytes.Buffer{}
+	writer := multipart.NewWriter(buf)
+
+	multi, err := writer.CreateFormFile("file", filepath.Base(path))
+	assertions.NoError(err)
+
+	_, err = io.Copy(multi, file)
+	assertions.NoError(err)
+
+	err = writer.Close()
+	assertions.NoError(err)
+
+	// when
+	res := httptest.NewRecorder()
+	req := httptest.NewRequest("POST", "/upload", buf)
+	req.Header.Set(echo.HeaderContentType, echo.MIMEMultipartForm)
+
+	// create new c echo.Context for testing
+	con := echo.New().NewContext(req, res)
+
+	// then
+	err = preprocess.UploadRecordingHandler(con)
+	assertions.NoError(err)
+
+	assert.Equal(t, http.StatusCreated, res.Code)
+}
